0c8d489a6455d670eb09379783451591a048b46f
Abinesh S <abinesh.s@sella.it>
2018-12-27 15:54:26 +0530

    add bubel

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,19 @@
-module.exports={
-    devServer:{
-        inline:true,
-        contentBase:'./src',
-        port:3000
+module.exports = {
+    mode: 'development',
+    entry: ['babel-polyfill', './src/client.js'],
+    output: {
+        filename: 'index.js'
     },
-    mode:'development'
+    devServer: {
+        inline: true,
+        contentBase: './public',
+        port: 3000
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    }
 };
