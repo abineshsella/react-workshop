660c974e4645aa57edb808f8f384792496f42131
Abinesh S <abinesh.s@sella.it>
2018-12-28 15:19:16 +0530

    header added

--- src/content.js
+++ src/content.js
@@ -2,6 +2,6 @@ import React from 'react';
 import GuttersGrid from './content/grid'
 export default class Content extends React.Component{
     render(){
-       return <div><GuttersGrid /> </div>;
+       return <div className="container special"><div className="jumbotron jumbotron-fulid"><GuttersGrid /> </div></div>;
     }
 }
--- src/header.js
+++ src/header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import SearchAppBar from './header/header-bar'
 export default class Header extends React.Component{
     render(){
-        return <div className="jumbotron jumbotron-fulid text-center">
-                     <div className="container">
-                        <h1>Promocode</h1>
-                     </div>
-                </div>;
+        // return <div className="jumbotron jumbotron-fulid text-center">
+        //              <div className="container">
+        //                 <h1>Promocode</h1>
+        //              </div>
+        //         </div>;
+        return <SearchAppBar />;
     }
 }
