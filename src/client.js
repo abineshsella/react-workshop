f89bdc10acb93df14c0a72c7b75b2fdf6d5d0d83
Abinesh S <abinesh.s@sella.it>
2019-04-17 16:23:45 +0530

    Dummy

--- src/client.js
+++ src/client.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import 'isomorphic-fetch';
+import 'isomorphic-fetch'; 
 ReactDOM.render(<App />,document.getElementById('root'));
